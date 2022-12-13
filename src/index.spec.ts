ef2c28c0ac5d24d8e64b35c199da9b6eb621376c
Adrian Albu <adrian.albu@and.digital>
2022-12-13 10:15:06 +0100

    lint-staged added

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
-const lib = require('./index')
+import lib from './index'
 
 test('it should work', () => {
-    expect(true).toEqual(true);
-    console.log(lib);
+  expect(true).toEqual(true)
+  console.log(lib)
 })
